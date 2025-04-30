79fece2d547ce007624e6469201a52884102b7cf
Lehmol <chalottelinnea@hotmail.com>
2025-04-30 10:10:51 +0200

    Installing router-dom and using browserrouter between the different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header.jsx"
+import Start from "./components/Start.jsx"
+import Quiz from "./components/Quiz.jsx"
+import ShowGame from "./components/ShowGame.jsx"
+
+
 const App = () => {
-  return <p>Hello World!</p>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Start />}/>
+        <Route path="/quiz" element={<Quiz />}/>
+        <Route path="/showgame" element={<ShowGame />}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
