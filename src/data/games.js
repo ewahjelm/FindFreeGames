6e11d7c1ababdb0661171347dabdbcde8de47621
Farah Najib <farahengnajib@gmail.com>
2025-05-02 15:37:59 +0200

    add mock data instead from fetch api due to cors issue

---
+++ src/data/games.js
@@ -0,0 +1,32 @@
+// src/api/games.js
+export const gamesData = [
+    {
+        id: 1,
+        title: 'Tic Tac Toe',
+        slug: 'tic-tac-toe',
+        description: 'Ett klassiskt tre-i-rad-spel för två spelare.',
+        image: '/images/placeholder.jpg',
+        type: 'Logik',
+        iframePath: '/games/tic-tac-toe/index.html'
+    },
+    {
+        id: 2,
+        title: 'Snake',
+        slug: 'snake',
+        description: 'Styr ormen, ät mat och väx utan att krocka.',
+        image: '/images/placeholder.jpg',
+        type: 'Arkad',
+        iframePath: '/games/snake/index.html'
+    },
+    {
+        id: 3,
+        title: 'Memory',
+        slug: 'memory',
+        description: 'Träna ditt minne genom att hitta matchande kort.',
+        image: '/images/placeholder.jpg',
+        type: 'Logik',
+        iframePath: '/games/memory/index.html'
+    },
+  
+
+]
