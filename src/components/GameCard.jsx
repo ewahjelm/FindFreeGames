d7e6de11a889e687cdeff1257bd913b1d6b01591
Farah Najib <farahengnajib@gmail.com>
2025-05-02 21:30:09 +0200

    update data

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,14 +1,14 @@
 const GameCard = ({game}) => {
     return (
-        <div  className="game-card">
+        <div className="game-card">
             <img
-                src={game.image}
+                src={game.thumbnail}
                 alt={game.title}
                 className="game-image"
                 width="150"
             />
             <h3>{game.title}</h3>
-            <p>{game.short_description}</p>
+            <p>{game.genre}</p>
         </div>
     )
 }
