d0d0e132edb10704c6b368b5e2a8cc714f1991b7
Your Name <you@example.com>
2025-05-05 15:29:02 +0200

    Wrote header "Genre"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,52 @@
-import GameGrid from '../components/GameGrid'
+import Footer from "../components/Footer";
+import GameGrid from "../components/GameGrid";
 
 const Home = () => {
-    const thisGenre = [
-        'Genre',
-        'mmorpg',
-        'Shooter',
-        'strategy',
-        'moba',
-        'racing',
-        'sports',
-        'social',
-        'sandbox',
-        'open-world',
-        'survival',
-        'pvp',
-        'pve'
-    ]
+  const thisGenre = [
+    "mmorpg",
+    "Shooter",
+    "strategy",
+    "moba",
+    "racing",
+    "sports",
+    "social",
+    "sandbox",
+    "open-world",
+    "survival",
+    "pvp",
+    "pve",
+  ];
 
+  return (
+    <div>
+      <header className="header">
+        <h2>Free to game</h2>
+      </header>
 
-    return (
-        <div>
-            <header className="header">
-                <h2>Free to game</h2>
-            </header>
+      <section className="section-grid">
+        <nav className="nav">
+          <ul>
+            <h3>Genre</h3>
+            <br />
+            {thisGenre.map((genre) => (
+              <li key={genre}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="genre-filter"
+                    value={genre.toLowerCase()}
+                  />
+                  {genre}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <GameGrid />
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
-            <section>
-                <nav className="nav">
-                    <ul>
-                        {thisGenre.map((genre) => (
-                            <li key={genre}>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        name="genre-filter"
-                                        value={genre.toLowerCase()}
-                                    />
-                                    {genre}
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <GameGrid />
-            </section>
-
-          
-        </div>
-    )
-}
-
-export default Home
+export default Home;
