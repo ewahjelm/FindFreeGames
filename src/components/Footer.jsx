4b4bab6446b40aedf20e09cd43db1ed494550450
Your Name <you@example.com>
2025-05-05 15:28:34 +0200

    Made a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="Footer">
+        <p className="footer-p">Project made of Team two</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
