2e2fa70df17f272036e77a0f69c34c3d53d1d0a9
Your Name <you@example.com>
2025-05-07 11:27:11 +0200

    CSS changes, fixed nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,60 +2,54 @@ import Footer from "../components/Footer";
 import GameGrid from "../components/GameGrid";
 
 const Home = () => {
-
-    const thisGenre = [
-        'Genre',
-        'mmorpg',
-        'Shooter',
-        'strategy',
-        'moba',
-        'racing',
-        'sports',
-        'social',
-        'sandbox',
-        'open-world',
-        'survival',
-        'pvp',
-        'pve',
-        'pixel',
-        'voxel',
-        'zombie',
-        'turn-based',
-        'first-person',
-        'third-person',
-        'top-down',
-        'tank',
-        'space',
-        'sailing',
-        'side-scroller',
-        'superhero',
-        'permadeath',
-        'card',
-        'battle-royale',
-        'mmo',
-        'mmofps',
-        'mmotps',
-        '3d',
-        '2d',
-        'anime',
-        'fantasy',
-        'sci-fi',
-        'fighting',
-        'action-rpg',
-        'action',
-        'military',
-        'martial-arts',
-        'flight',
-        'low-spec',
-        'tower-defense',
-        'horror',
-        'mmorts'
-
-    ]
-    const thisPlatform = [
-        'Browser',
-        'PC'
-    ]
+  const thisGenre = [
+    "mmorpg",
+    "Shooter",
+    "strategy",
+    "moba",
+    "racing",
+    "sports",
+    "social",
+    "sandbox",
+    "open-world",
+    "survival",
+    "pvp",
+    "pve",
+    "pixel",
+    "voxel",
+    "zombie",
+    "turn-based",
+    "first-person",
+    "third-person",
+    "top-down",
+    "tank",
+    "space",
+    "sailing",
+    "side-scroller",
+    "superhero",
+    "permadeath",
+    "card",
+    "battle-royale",
+    "mmo",
+    "mmofps",
+    "mmotps",
+    "3d",
+    "2d",
+    "anime",
+    "fantasy",
+    "sci-fi",
+    "fighting",
+    "action-rpg",
+    "action",
+    "military",
+    "martial-arts",
+    "flight",
+    "low-spec",
+    "tower-defense",
+    "horror",
+    "mmorts",
+  ];
+  const thisPlatform = ["Browser", "PC"];
 
   return (
     <div>
@@ -63,46 +57,46 @@ const Home = () => {
         <h2>Free to game</h2>
       </header>
 
-            <section>
-                <nav className="nav">
-                    <p>Platform</p>
-                    <ul>
-                        {thisPlatform.map((platform) => (
-                            <li key={platform}>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        name="platform-filter"
-                                        value={platform.toLowerCase()}
-                                    />
-                                    {platform}
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-                    <p>Genre</p>
-                    <ul>
-                        {thisGenre.map((genre) => (
-                            <li key={genre}>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        name="genre-filter"
-                                        value={genre.toLowerCase()}
-                                    />
-                                    {genre}
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <GameGrid />
-            </section>
+      <div className="main-layout">
+        <nav className="nav">
+          <p>Platform</p>
+          <br />
+          <ul>
+            {thisPlatform.map((platform) => (
+              <li key={platform}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="platform-filter"
+                    value={platform.toLowerCase()}
+                  />
+                  {platform}
+                </label>
+              </li>
+            ))}
+          </ul>
+          <br />
+          <p>Genre</p>
+          <br />
+          <ul>
+            {thisGenre.map((genre) => (
+              <li key={genre}>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="genre-filter"
+                    value={genre.toLowerCase()}
+                  />
+                  {genre}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <GameGrid />
+      </div>
+    </div>
+  );
+};
 
-          
-        </div>
-    )
-}
-
-export default Home
-
+export default Home;
