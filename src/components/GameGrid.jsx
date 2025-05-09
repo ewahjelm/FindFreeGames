de313843344390aeb4c697212545504e50a4f200
Lehmol <chalottelinnea@hotmail.com>
2025-05-09 11:07:01 +0200

    Made filtering by genre and platform available

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,19 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { gamesData } from '../data/games' // Importing the mock data
-import GameCard from '../components/GameCard'
-const GameGrid = () => {
-     const [games, setGames] = useState([])
+import React, { useEffect, useState } from "react";
+import { gamesData } from "../data/games"; // Importing the mock data
+import GameCard from "../components/GameCard";
 
-        useEffect(() => {
-            setGames(gamesData) // Mock fetching games
-        }, [])
-    return (
-        <div className="game-list">
-            {games.map((game) => (
-                <GameCard key={game.id} game={game} />
-            ))}
-        </div>
-    )
-}
+const GameGrid = ({ selectedGenre, selectedPlatform }) => {
+  const [games, setGames] = useState([]);
 
-export default GameGrid
+  useEffect(() => {
+    setGames(gamesData); // Mock fetching games
+  }, []);
+
+  //Filter games by genre and platform
+  const filteredGames = games.filter((game) => {
+    const genre = game.genre.toLowerCase();
+    const platform = game.platform.toLowerCase();
+
+    const genreMatch =
+      selectedGenre.length === 0 || selectedGenre.includes(genre);
+    const platformMatch =
+      selectedPlatform.length === 0 ||
+      selectedPlatform.some((p) => platform.includes(p));
+
+    return genreMatch && platformMatch;
+  });
+
+  return (
+    <div className="game-list">
+      {filteredGames.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </div>
+  );
+};
+
+export default GameGrid;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 import Footer from "../components/Footer";
 import GameGrid from "../components/GameGrid";
 
 const Home = () => {
+  const [selectedGenre, setSelectedGenre] = useState([]);
+  const [selectedPlatform, setSelectedPlatform] = useState([]);
+
   const thisGenre = [
     "mmorpg",
     "Shooter",
@@ -51,6 +56,27 @@ const Home = () => {
   ];
   const thisPlatform = ["Browser", "PC"];
 
+  // If the user checks one of the checkboxes,
+  // the handler adds the tag into selectedGenre/selectedPlatform.
+  // Once it's unchecked, the handler removes the tag from selectedGenre/selectedPlatform
+  const GenreChange = (e) => {
+    const value = e.target.value;
+    if (e.target.checked) {
+      setSelectedGenre([...selectedGenre, value]);
+    } else {
+      setSelectedGenre(selectedGenre.filter((g) => g !== value));
+    }
+  };
+
+  const PlatformChange = (e) => {
+    const value = e.target.value;
+    if (e.target.checked) {
+      setSelectedPlatform([...selectedPlatform, value]);
+    } else {
+      setSelectedPlatform(selectedPlatform.filter((p) => p !== value));
+    }
+  };
+
   return (
     <div>
       <header className="header">
@@ -69,6 +95,7 @@ const Home = () => {
                     type="checkbox"
                     name="platform-filter"
                     value={platform.toLowerCase()}
+                    onChange={PlatformChange}
                   />
                   {platform}
                 </label>
@@ -86,6 +113,7 @@ const Home = () => {
                     type="checkbox"
                     name="genre-filter"
                     value={genre.toLowerCase()}
+                    onChange={GenreChange}
                   />
                   {genre}
                 </label>
@@ -93,8 +121,12 @@ const Home = () => {
             ))}
           </ul>
         </nav>
-        <GameGrid />
+        <GameGrid
+          selectedGenre={selectedGenre}
+          selectedPlatform={selectedPlatform}
+        />
       </div>
+      <Footer />
     </div>
   );
 };
