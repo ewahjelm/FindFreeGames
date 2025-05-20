4c9fd6353cbe21790501004cec8b3dc91bf30f9f
Your Name <you@example.com>
2025-05-20 16:35:13 +0200

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // L�
 import Start from "./pages/Start";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import "./App.css";
 
 const App = () => {
   const [selectedGame, setSelectedGame] = useState(null); // ⬅️ Flytta hit
