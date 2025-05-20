31ebc3658e2426f6a23661e988de21959d41e64b
Farah Najib <farahengnajib@gmail.com>
2025-05-20 10:42:01 +0200

    change the style

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -2,22 +2,32 @@ import React from "react";
 
 function Start() {
   return (
-    <div className="content">
-      <div className="left-side">
-        <img src="./public/bg.jpg" alt="gamer-illustration" />
+      <div className="content">
+ <div className="header-start">
+              <h1 className="content-header">FIND FREE GAMES TO PLAY</h1>
+</div>
+          <div className="content-body">
+
+              <img src="./public/bg.jpg" alt="gamer-illustration" />
+
+              <div className="right">
+                  <p className="subtitle">
+                      We track the best free PC and browser games <br /> so you
+                      don’t have to
+                  </p>
+                  <button className="start-btn">Browse</button>
+              </div>
+          </div>
+
+          {/* <div className="right-side">
+              <p className="subtitle">
+                  We track the best free PC and browser games <br /> so you
+                  don’t have to
+              </p>
+              <button className="start-btn">Browse</button>
+          </div> */}
       </div>
-      <div className="center-side">
-        <h1>FIND FREE GAMES TO PLAY</h1>
-      </div>
-      <div className="right-side">
-        <p className="subtitle">
-          We track the best free PC and browser games <br /> so you don’t have
-          to
-        </p>
-        <button className="start-btn">Browse</button>
-      </div>
-    </div>
-  );
+  )
 }
 
 export default Start;
