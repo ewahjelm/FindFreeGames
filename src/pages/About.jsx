6da115fd2bf4d73845cfa7f9d6ea0e655845debc
Ewa <ewahjelm84@gmail.com>
2025-05-22 01:21:08 +0200

    småfix i about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,26 +6,28 @@ function About() {
     <div>
       <Header />
       <div className="content">
-        <p className="content-header">About</p>
+        {/* <h1 className="content-header">Let us tell you about this site...</h1> */}
         <div className="content-body">
           <img src="./public/girl.jpg" />
 
           <div className="right">
             <h3 className="standard-h3">Have you ever grown tired of a game you used to love? <br /></h3>
             <p className="p-text">
-              Felt there might be a new fun game out there but don't know where to look?<br />
-              Then you're in the right place, my friend! 
-              </p><p className="p-text">
+              Felt there might be a new fun game out there but don't know where to look?
+              Then you're in the right place, my friend!
+            </p>
+            <p className="p-text">
               What would you say if I told you that you could play for free? <br />
               Our site is powered by the FreeToGame API, which provides a
               continuously updated list of free-to-play games available for PC and web
               browsers from several official publishers and platforms. And no, these are not the only free games in the world. There are many others, but FreeToGame focuses on high-quality, multiplayer, and online games that are completely free to play (not just demos or trials). Isn’t it great!
               Whether you're into action, RPGs, strategy, or casual games, we
               make it easy to browse, filter, and find games that match your
-              preferences. We don’t host any games ourselves — instead, we direct you
+              preferences. We don't host any games ourselves — instead, we direct you
               to the official game pages so that you can start playing right
-              away. This service brings you free games made accessible for everyone. <br />
-              Our mission is simple: To make free
+              away. </p>
+            <h3 className="standard-h3">We bring you free games made accessible for everyone</h3>
+            <p className="p-text">Our mission is simple: To make free
               gaming easier to find and accessible to everyone. Enjoy!
 
             </p>
