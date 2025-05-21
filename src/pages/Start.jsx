bb56e1b3e80823833724396c18e2c8a3e25d3af0
Lehmol <chalottelinnea@hotmail.com>
2025-05-21 12:37:10 +0200

    Changes in responsive design

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -3,32 +3,33 @@ import { Link } from "react-router-dom";
 
 function Start() {
   return (
-      <div className="content">
- <div className="header-start">
-              <h1 className="content-header">FIND FREE GAMES TO PLAY</h1>
-</div>
-          <div className="content-body">
+    <div className="content">
+      <div className="header-start">
+        <h1 className="header-content">FIND FREE GAMES TO PLAY</h1>
+      </div>
+      <div className="content-body">
+        <img src="./public/bg.jpg" alt="gamer-illustration" />
 
-              <img src="./public/bg.jpg" alt="gamer-illustration" />
+        <div className="right">
+          <p className="subtitle">
+            We track the best free PC and browser games <br /> so you don’t have
+            to
+          </p>
+          <Link to="/home" className="start-btn">
+            Browse
+          </Link>
+        </div>
+      </div>
 
-              <div className="right">
-                  <p className="subtitle">
-                      We track the best free PC and browser games <br /> so you
-                      don’t have to
-                  </p>
-                  <Link to="/home" className="start-btn">Browse</Link>
-              </div>
-          </div>
-
-          {/* <div className="right-side">
+      {/* <div className="right-side">
               <p className="subtitle">
                   We track the best free PC and browser games <br /> so you
                   don’t have to
               </p>
               <button className="start-btn">Browse</button>
           </div> */}
-      </div>
-  )
+    </div>
+  );
 }
 
 export default Start;
