faed6624b044c4f414da3ab972d289e5b6cfce26
Ewa <ewahjelm84@gmail.com>
2025-05-21 23:25:18 +0200

    startbutton text and code cleanup

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -2,33 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Start() {
-  return (
-      <div className="content">
- <div className="header-start">
-              <h1 className="content-header-start">FIND FREE GAMES TO PLAY</h1>
-</div>
-          <div className="content-body">
+    return (
+        <div className="content">
+            <div className="header-start">
+                <h1 className="content-header-start">FIND FREE GAMES TO PLAY</h1>
+            </div>
+            <div className="content-body">
 
-              <img src="./public/bg.jpg" alt="gamer-illustration" />
+                <img src="./public/bg.jpg" alt="gamer-illustration" />
 
-              <div className="right">
-                  <p className="subtitle">
-                      We track the best free PC and browser games <br /> so you
-                      don’t have to
-                  </p>
-                  <Link to="/home" className="start-btn">Browse</Link>
-              </div>
-          </div>
+                <div className="right">
+                    <p className="subtitle">
+                        We track the best free PC and browser games <br /> so you
+                        don’t have to
+                    </p>
+                    <Link to="/home" className="start-btn">Great! Let's go!</Link>
+                </div>
+            </div>
 
-          {/* <div className="right-side">
-              <p className="subtitle">
-                  We track the best free PC and browser games <br /> so you
-                  don’t have to
-              </p>
-              <button className="start-btn">Browse</button>
-          </div> */}
-      </div>
-  )
+        </div>
+    )
 }
 
 export default Start;
