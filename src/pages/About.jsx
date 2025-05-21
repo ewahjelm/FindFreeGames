f5342a7dc8badce76e37c7891631138360ef1e8b
Ewa <ewahjelm84@gmail.com>
2025-05-21 23:13:03 +0200

    About text edited

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,26 +3,31 @@ import Footer from "../components/Footer";
 
 function About() {
   return (
-      <div>
-          <Header />
-          <div className="content">
-              <p className="content-header">About</p>
-              <div className="content-body">
-                  <img src="./public/girl.jpg" />
+    <div>
+      <Header />
+      <div className="content">
+        <p className="content-header">About</p>
+        <div className="content-body">
+          <img src="./public/girl.jpg" />
 
 
           <div className="right">
             <p className="subtitle">
+              Have you ever grown tired of a game you used to love? Then felt
+              there might be a new fun game out there but don't know where to look?
+              Then you're in the right place, my friend! <br />
+              What would you say if I told you that you could play for free? <br />
               Our site is powered by the FreeToGame API, which provides a
-              constantly updated list of free games available for PC and web
-              browsers. <br />
+              continuously updated list of free-to-play games available for PC and web
+              browsers from several official publishers and platforms. And no, these are not the only free games in the world. There are many others, but FreeToGame focuses on high-quality, multiplayer, and online games that are completely free to play (not just demos or trials). Isn’t it great!
               Whether you're into action, RPGs, strategy, or casual games, we
               make it easy to browse, filter, and find games that match your
-              style. We don’t host any games ourselves — instead, we connect you
-              directly to the official game pages so you can start playing right
-              away. No hidden costs, no sign-ups, just free games curated and
-              made accessible for everyone. Our mission is simple: make free
-              gaming easier to find and more fun to explore.
+              preferences. We don’t host any games ourselves — instead, we direct you
+              to the official game pages so that you can start playing right
+              away. This service brings you free games made accessible for everyone. <br />
+              Our mission is simple: To make free
+              gaming easier to find and accessible to everyone. Enjoy!
+
             </p>
           </div>
         </div>
